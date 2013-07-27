29767984a1cffcf84a245fe811d9586a4e22934c
wtbeaver <wtbeaver@ncsu.edu>
2013-07-27 15:14:10 -0400

    added posts to the routes and wrote a beginning integration test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe "Posts" do
   describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get posts_index_path
-      response.status.should be(200)
+    it "display some tasks" do
+      visit tasks_path
+
     end
   end
 end
