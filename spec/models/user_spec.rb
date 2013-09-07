f2ada0967d383e8c582ea36cb46aa7339c7e5633
wtbeaver <wtbeaver@ncsu.edu>
2013-09-07 18:29:20 -0400

    Added tests and code for creating a valid user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
-
+#  before do
+#    @user = User.create :name => 'test', :content => 'pass'
+#    @no_ps_user = User.create :name => 'bad_pass', :content => ''
+#  end
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new user" do
+    user = User.new :name => 'new', :password => 'pass', :password_confirmation => 'pass'
+    user.should be_valid
+  end
+  it "should require a password" do
+    user = User.new :name => 'new'
+    user.should_not be_valid
+  end
+
 end
