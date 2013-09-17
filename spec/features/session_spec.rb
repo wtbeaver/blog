bb9de9bfa4712a6a5534f03f956f4c75642dd01b
wtbeaver <wtbeaver@ncsu.edu>
2013-09-16 23:24:49 -0400

    Added new tests for creating a user session

---
+++ spec/features/session_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Posts" do
+  before do
+    @user = User.create :name => 'first_user', :password => 'first_password', :password_confirmation => 'first_password'
+  end
+  describe "GET /sessions" do
+    it "create a new session" do
+      visit log_in
+      fill_in 'user_name', :with => 'first_user'
+      fill_in 'user_password', :with => 'first_password'
+      click_button 'Sign In'
+      
+      current_path.should == log_in
+      page.should have_content 'signed in'
+
+    end
+  end
+=begin
+
+  describe "DELETE /posts" do
+    it "should delete a session" do
+      visit posts_path
+      find("#post_#{@post.id}").click_link 'Delete'
+      page.should have_content 'Post has been deleted'
+      page.should have_no_content 'First Post'
+      
+    end
+  end
+=end
+end
